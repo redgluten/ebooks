0e8500530702d22a79399dcd5353fa18458f45d5
gluten <goulvenschaal@me.com>
2014-09-07 18:43:53 +0200

    balise meta pour affichage responsive

--- head.php
+++ head.php
@@ -1,5 +1,6 @@
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width" />
 	<title>eBooks Project | <?= $eBook->getTitle();  ?></title>
 	<link rel="stylesheet" href="js/main-min.js">
 	<link rel="stylesheet" href="css/main-min.css">
